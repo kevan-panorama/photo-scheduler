d8a0dbaa2aef8c5b2d5da95a040232929a65beee
kevan-panorama <kevan@panorama.es>
2026-05-18 15:26:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
-@import "tailwindcss";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-* {
-  box-sizing: border-box;
-}
-
-html,
-body {
-  margin: 0;
-  padding: 0;
-  background: #f4f1ea;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-button,
-input,
-select,
-textarea {
-  font-family: inherit;
-}
+module.exports = nextConfig;
